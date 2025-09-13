868387a38bdab97b52bdff136244b0828f6a730e
Jcano <jcano2620k@gmail.com>
2025-09-12 21:09:31 -0500

    prosedimiendos yfunciones sql

--- init.sql
+++ init.sql
@@ -68,7 +68,8 @@ CREATE TABLE Examen (
 CREATE TABLE Pregunta (
     id_pregunta SERIAL PRIMARY KEY,
     texto_pregunta VARCHAR,
-    tipo_pregunta VARCHAR
+    tipo_pregunta VARCHAR,
+    contenido VARCHAR
 );
 
 -- ======================================
@@ -87,7 +88,8 @@ CREATE TABLE examen_pregunta (
 -- ======================================
 CREATE TABLE Respuesta (
     id_respuesta SERIAL PRIMARY KEY,
-    valor_respuesta VARCHAR(30),
+    texto_respuesta VARCHAR,
+    valor_respuesta VARCHAR,
     id_aspirante INT,
     id_pregunta INT,
     id_examen INT,
@@ -134,3 +136,104 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--======================================
+-- procedimiento para insertar examen nombre : insertar_examen
+--======================================
+
+CREATE OR REPLACE PROCEDURE insertar_examen(
+    p_nombre        VARCHAR,
+    p_fecha_inicio  TIMESTAMP,
+    p_fecha_fin     TIMESTAMP,
+    
+    p_estado        VARCHAR,
+    p_id_instructor INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Examen(
+        nombre,
+        fecha_inicio,
+        fecha_fin,
+        habilitado,
+        estado,
+        id_instructor
+    )
+    VALUES (
+        p_nombre,
+        p_fecha_inicio,
+        p_fecha_fin,
+        FALSE,
+        p_estado,
+        p_id_instructor
+    );
+END;
+$$;
+--======================================
+-- procedimiento para insertar aspirante nombre : insertar_aspirante
+--======================================
+
+
+CREATE OR REPLACE PROCEDURE insertar_aspirante(
+    p_primer_nombre    VARCHAR,
+    p_segundo_nombre   VARCHAR,
+    p_primer_apellido  VARCHAR,
+    p_segundo_apellido VARCHAR,
+    p_email            VARCHAR,
+    p_tipo_tarjeta     tipo_documento,
+    p_numero_tarjeta   BIGINT,
+    p_contrasena       VARCHAR,
+    p_id_ficha         INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Aspirante(
+        primer_nombre,
+        segundo_nombre,
+        primer_apellido,
+        segundo_apellido,
+        email,
+        tipo_tarjeta,
+        numero_tarjeta,
+        contrasena,
+        id_ficha
+    )
+    VALUES (
+        p_primer_nombre,
+        p_segundo_nombre,
+        p_primer_apellido,
+        p_segundo_apellido,
+        p_email,
+        p_tipo_tarjeta,
+        p_numero_tarjeta,
+        p_contrasena,
+        p_id_ficha
+    );
+END;
+$$;
+
+--======================================
+-- procedimiento para insertar ficha nombre : insertar_ficha
+--======================================
+
+CREATE OR REPLACE PROCEDURE insertar_ficha(
+    p_nombre_ficha VARCHAR,
+    p_codigo_ficha BIGINT,
+    p_id_instructor INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Ficha(
+        nombre_ficha,
+        codigo_ficha,
+        id_instructor
+    )
+    VALUES (
+        p_nombre_ficha,
+        p_codigo_ficha,
+        p_id_instructor
+    );
+END;
+$$;
